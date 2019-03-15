ea37b960c8e9c05719d2bc28eecab28810a2c3c7
danik <dan_ntu@mail.ru>
2019-03-15 10:42:11 +0600

    Defining Routes with @Route

---
+++ src/main/java/com/example/route/SomePathComponent.java
@@ -0,0 +1,11 @@
+package com.example.route;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("some/path")
+public class SomePathComponent extends Div {
+    public SomePathComponent() {
+        setText("Hello @Route");
+    }
+}
